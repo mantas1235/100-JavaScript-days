ea1c28c4800905135219bb8d695bfbf1e001fc3b
Mantas Santockis <mantas.san@gmail.com>
2023-04-19 11:19:18 +0300

    input character finish

---
+++ Input Field Character Count/script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const count = document.querySelector(".count")
+const input = document.querySelector("input")
+
+
+
+
+input.addEventListener("keyup", ()=> {
+    count.innerHTML= input.value.length
+})
+
+
