dde52316c382bc24309e1e7b29face81b4b2b354
Mantas Santockis <mantas.san@gmail.com>
2023-04-26 20:29:16 +0300

    finish search bar

--- search bar/script.js
+++ search bar/script.js
@@ -1,1 +1,30 @@
-"use"
+"use strict"
+
+const input = document.getElementById("search")
+
+
+input.addEventListener("keyup", search)
+
+
+
+function search() {
+    const inputVal = input.value
+
+const li = document.getElementsByTagName("li")
+
+for (let i = 0; i < li.length; i++) {
+    if (li[i].innerHTML.toLowerCase().includes(inputVal)) {
+        
+li[i].style.display = ""
+
+
+    } else {
+
+        li[i].style.display = "none" 
+
+    }
+    
+}
+
+
+}
