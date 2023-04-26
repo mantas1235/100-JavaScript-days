565fb272d1b94e53f2bfd6e850d5e32756bacaee
Mantas Santockis <mantas.san@gmail.com>
2023-04-26 21:29:58 +0300

    finish app

---
+++ coundown app/script.js
@@ -0,0 +1,38 @@
+"use strict"
+
+
+const countTo = "27 Apr 2023"
+
+const  c = setInterval(()=> {
+
+const endDate = new Date(countTo)
+const currentDate = new Date()
+
+const totalSeconds = (endDate - currentDate)  / 1000;
+
+const days = Math.floor(totalSeconds/3600/24)
+
+const hours = Math.floor(totalSeconds/3600) % 24
+const minutes = Math.floor(totalSeconds/60) % 60
+const seconds = Math.floor(totalSeconds) % 60
+
+const countDown = document.getElementById("countdown")
+
+
+countDown.textContent = `${days}Days ${format(hours)} Hours : ${format(minutes)} Minutes : ${format(seconds)} Sec`
+
+
+if (totalSeconds < 0) {
+clearInterval(c)
+
+countDown.textContent = "Happy New Year";
+
+}
+
+
+}, 1000)
+
+
+function format(time) {
+return time<10 ? `0${time}` : time
+}
