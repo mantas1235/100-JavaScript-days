6ebd96c83a0e004446ebba3bad3201bbd79c18b6
Mantas Santockis <mantas.san@gmail.com>
2023-05-24 21:42:21 +0300

    finish logic for sticky navbar

---
+++ sticky navBar/script.js
@@ -0,0 +1,32 @@
+const links = document.querySelectorAll(".nav-list li a")
+
+
+for (const link of links) {
+
+    link.addEventListener("click", smoothScroll)
+    
+}
+
+
+
+function smoothScroll(e) {
+    e.preventDefault()
+
+const href = this.getAttribute("href")
+
+
+document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+}
+)
+}
+
+
+
+// sticky Header
+
+window.addEventListener("scroll", ()=> {
+    const header = document.querySelector("header")
+
+    header.classList.toggle("sticky", window.scrollY >0)
+})
