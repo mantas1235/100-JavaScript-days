0875442af81f6d052420f5c28aff8cdcbbc3a9ba
Mantas Santockis <mantas.san@gmail.com>
2023-06-13 20:55:49 +0300

    add active class

--- sticky navBar/script.js
+++ sticky navBar/script.js
@@ -1,5 +1,5 @@
 const links = document.querySelectorAll(".nav-list li a")
-
+const navList = document.querySelector(".nav-list")
 
 for (const link of links) {
 
@@ -29,4 +29,16 @@ window.addEventListener("scroll", ()=> {
     const header = document.querySelector("header")
 
     header.classList.toggle("sticky", window.scrollY >0)
+})
+
+//Active menu swicher
+
+navList.addEventListener("click", (e)=> {
+    e.preventDefault()
+const navLi = e.target.parentElement;
+
+    if (navLi.classList.contains("link")) {
+       navList.querySelector(".active").classList.remove("active")
+       navLi.classList.add("active")
+    }
 })
