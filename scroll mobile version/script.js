049f5b52825f8451a939a8af4e10474c0d399540
Mantas Santockis <mantas.san@gmail.com>
2023-06-14 22:24:04 +0300

    create a mobile response page

---
+++ scroll mobile version/script.js
@@ -0,0 +1,89 @@
+const links = document.querySelectorAll(".nav-list li a")
+const btn = document.querySelector(".scroll")
+const rootEl = document.documentElement
+
+
+
+
+
+document.addEventListener("scroll", showBtn)
+btn.addEventListener("click", scrollToTop)
+
+function showBtn() {
+    const scrollTotal = rootEl.scrollHeight - rootEl.clientHeight
+
+if (rootEl.scrollTop/ scrollTotal > 0.3) {
+    btn.classList.add("showBtn")
+}
+
+else{
+    btn.classList.remove("showBtn")
+}
+
+
+}
+
+
+
+function scrollToTop() {
+    rootEl.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+
+
+
+
+for (const link of links) {
+
+    link.addEventListener("click", smoothScroll)
+    
+}
+
+
+
+function smoothScroll(e) {
+    e.preventDefault()
+
+const href = this.getAttribute("href")
+
+
+document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+}
+)
+
+hideMenu()
+}
+
+
+//Mobile responsive menu
+
+const menu =document.querySelector(".nav-list")
+const hamburger =document.querySelector(".hamburger")
+const close =document.querySelector(".close")
+
+
+
+const showMenu = () => {
+    hamburger.style.display ="none"
+    close.style.transform = "translateY(0)"
+    menu.style.transform = "translateY(0)"
+
+}
+
+
+const hideMenu = () => {
+    hamburger.style.display ="block"
+    close.style.transform = "translateY(-20rem)"
+    menu.style.transform = "translateY(-200%)"
+}
+
+
+hamburger.addEventListener("click", showMenu)
+close.addEventListener("click", hideMenu)
+
+
+
