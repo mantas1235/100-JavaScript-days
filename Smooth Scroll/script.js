648d1f2916eb966e16fee15e110ad6f9585cc2c8
Mantas Santockis <mantas.san@gmail.com>
2023-06-14 21:31:02 +0300

    add button to scroll to top

--- Smooth Scroll/script.js
+++ Smooth Scroll/script.js
@@ -1,4 +1,39 @@
 const links = document.querySelectorAll(".nav-list li a")
+const btn = document.querySelector(".scroll")
+const rootEl = document.documentElement
+
+
+
+
+
+document.addEventListener("scroll", showBtn)
+btn.addEventListener("click", scrollToTop)
+
+function showBtn() {
+    const scrollTotal = rootEl.scrollHeight - rootEl.clientHeight
+
+if (rootEl.scrollTop/ scrollTotal > 0.3) {
+    btn.classList.add("showBtn")
+}
+
+else{
+    btn.classList.remove("showBtn")
+}
+
+
+}
+
+
+
+function scrollToTop() {
+    rootEl.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+
+
 
 
 for (const link of links) {
