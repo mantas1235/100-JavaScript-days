78678c3b5f32ca826f2a196aa926ac136d2a634e
Mantas Santockis <mantas.san@gmail.com>
2023-07-12 09:59:24 +0300

    finish local storage

--- local storage/script.js
+++ local storage/script.js
@@ -10,4 +10,36 @@
 // window.localStorage.setItem("key", "value");
 const name = window.localStorage.setItem("FirstName", "Mantas");
 
+localStorage.setItem("lastName", "San")
 
+
+const person = {
+    fullName: "Mantas San",
+    age: 31,
+    location: "Kaunas"
+}
+
+
+localStorage.setItem("user", JSON.stringify(person))
+
+const fruits = ["Apple", "banana", "Mango", "pear"]
+
+localStorage.setItem("fruits", JSON.stringify(fruits))
+
+//Get item from local storage
+
+
+console.log(localStorage.getItem("FirstName"));
+
+
+
+//REMOVE item from local storage
+
+
+localStorage.removeItem("fruits")
+
+localStorage.clear()
+
+localStorage.setItem("name", "Mantas")
+localStorage.setItem("age", "31")
+console.log(localStorage.key(0));
