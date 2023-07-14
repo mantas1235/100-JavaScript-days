ec7e5e260e487ba26c3cc5945351d7f1f9001cfb
Mantas Santockis <mantas.san@gmail.com>
2023-07-14 19:09:59 +0300

    finish simple calculator

---
+++ calculator/script.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const keys = document.getElementsByTagName("li")
+const result  = document.querySelector("p")
+const clear = document.querySelector(".clear")
+
+
+for (let i = 0; i < keys.length; i++) {
+  if(keys[i].innerHTML=== "=") {
+    keys[i].addEventListener("click", calculate)
+  }
+  else{
+    keys[i].addEventListener("click", addToValue(i))
+  }
+    
+}
+
+
+function addToValue(i) {
+    return ()=> {
+       
+     if (keys[i].innerHTML=== "x"){
+            result.innerHTML+= "* "
+        }
+        else{
+            result.innerHTML+=keys[i].innerHTML
+        }
+    }
+}
+
+
+function calculate() {
+    return (result.innerHTML = eval(result.innerHTML))
+}
+
+clear.addEventListener("click", ()=> {
+    result.innerHTML = ""
+})
